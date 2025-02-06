d2c885ee7661257c61e437405c7d94ce28a04f97
Alexander Kern <alexander.t.kern@gmail.com>
2025-02-06 15:15:19 -0600

    board scaffolding setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_06_160241) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_06_211218) do
+  create_table "boards", force: :cascade do |t|
+    t.string "owner"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "quests", force: :cascade do |t|
     t.string "title"
     t.text "description"
