14597e17f5d764cce70adc1f20ebe431c4b0010a
Alexander Kern <alexander.t.kern@gmail.com>
2025-02-12 15:00:15 -0600

    quest completed check and default task complete false

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,4 +1,12 @@
 class Quest < ApplicationRecord
     belongs_to :board
     belongs_to :author, class_name: "User"
+
+    has_many :tasks, dependent: :destroy
+
+
+
+    def completed?
+        tasks.all?(&:completed)
+    end
 end
