ced7b4b4a33bef3f6d6812e7db823a225c6a279c
Alexander Kern <alexander.t.kern@gmail.com>
2025-02-18 14:26:24 -0600

    db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_231838) do
     t.index ["target_user_id"], name: "index_quests_on_target_user_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.text "description"
     t.boolean "completed", default: false
@@ -44,16 +53,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_231838) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   add_foreign_key "boards", "users"
   add_foreign_key "quests", "boards"
   add_foreign_key "quests", "users", column: "author_id"
   add_foreign_key "quests", "users", column: "target_user_id"
+  add_foreign_key "sessions", "users"
   add_foreign_key "tasks", "quests"
 end
