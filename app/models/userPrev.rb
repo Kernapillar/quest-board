43022592e3f99f8f61a4e370ac55d7446c6de097
Alexander Kern <alexander.t.kern@gmail.com>
2025-02-24 17:11:58 -0600

    userPrev cleanup

--- app/models/userPrev.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    has_one :board
-    has_many :quests, foreign_key: :author_id
-
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-    validates :password_digest, presence: true, length: { minimum: 6 }
-end
